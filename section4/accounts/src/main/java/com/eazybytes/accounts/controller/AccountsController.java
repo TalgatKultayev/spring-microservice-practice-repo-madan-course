4215d6d3ab4e19054e1150bbce5b5c840abd4e57
TalgatKultayev <kultayevtalgat@gmail.com>
2023-08-23 21:57:43 +0600

    commit #5. Making microservices resilient

--- section4/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ section4/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -10,6 +10,10 @@ import com.eazybytes.accounts.model.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,6 +68,10 @@ public class AccountsController {
 	}
 
 	@PostMapping("/myCustomerDetails")
+	@CircuitBreaker(name = "detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
+	@Retry(name = "retryForCustomerDetails", fallbackMethod = "myCustomerDetailsFallBack")
+	@RateLimiter(name="detailsForCustomerSupportApp", fallbackMethod = "myCustomerDetailsFallBack")
+	@Bulkhead(name="bulkheadAccounts", fallbackMethod = "bulkheadAccountsFallBack")
 	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
 		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 		List<Loans> loans = loansFeignClient.getLoanDetails(customer);
@@ -73,6 +81,17 @@ public class AccountsController {
 		customerDetails.setAccounts(accounts);
 		customerDetails.setLoans(loans);
 		customerDetails.setCards(cards);
+
+		return customerDetails;
+	}
+
+	private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable throwable) {
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoanDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
 
 		return customerDetails;
 	}
